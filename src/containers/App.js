75d8af6e8b180980354e3ef1097235fb199646a7
cupatea <vdavyden@gmail.com>
2017-12-11 22:41:05 +0200

    Fix scroll issue

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,11 +9,16 @@ import Categories from './Categories'
 import Products from './Products'
 
 class App extends Component {
+  ScrollToTop() {
+    window.scrollTo(0, 0)
+    return null
+  }  
 
   render() {
     const currentTabSlug = this.props.location.pathname.split('/')[1]
     return(
       <div>
+        <Route component = { this.ScrollToTop } />
         <Header appName = { appName } />
         <Tabs tabs = { categories } currentTab = { currentTabSlug } />
         <Switch>
